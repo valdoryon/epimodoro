222e88e78a19db128e91d3cd87e7273a457ef41d
valdoryon <valdoryon@gmail.com>
2024-01-14 12:28:43 -0300

    fixed footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import { RiCopyrightLine } from 'react-icons/ri';
+
+const Footer = () => {
+  return (
+    <footer className='footer-main_container'>
+      <span className='footer-text'>
+        Epimodoro - All Rights Reserved 2024
+        <RiCopyrightLine />
+      </span>
+    </footer>
+  );
+};
+
+export default Footer;
